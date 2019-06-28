b8ba21dba7961db7cf57964b9f798a1fb3e53010
AJK55 <40635902+AJK55@users.noreply.github.com>
2019-06-28 11:48:34 -0400

    Add files via upload

---
+++ mercadoRun.bat
@@ -0,0 +1,11 @@
+cd..
+cd..
+cd..
+cd Users
+cd aklinga
+cd desktop
+cd Tools_Applications
+cd apache-jmeter-5.1.1
+cd bin
+jmeter -n -t examples\tests\Mercado\mercadobitcoin.jmx -l examples\tests\Mercado\mtestresult.jtl
+call examples\tests\Mercado\rename.bat
